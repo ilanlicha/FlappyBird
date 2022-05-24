48c4c1bfba2c12bddee4e668a30a0356fab7166b
admin <Juliettemimenne42>
2022-05-24 15:06:05 +0200

    flappy rajout images

---
+++ assets/assets.ino
@@ -0,0 +1,14 @@
+#ifndef PLATFORMER_CONSTANTS
+#define PLATFORMER_CONSTANTS
+
+#include <Gamebuino-Meta.h>
+
+// Pour un personnage de 8 pixels de haut sur 6 pixels de large
+const uint8_t WIDTH_HERO = 6;
+const uint8_t HEIGHT_HERO = 8;
+const uint8_t UNDER_CENTER_X_HERO = 3; // à droite du héro
+const uint8_t UNDER_CENTER_Y_HERO = 4; // en dessous du héro
+const uint8_t OVER_CENTER_X_HERO = 3; // à gauche du héro
+const uint8_t OVER_CENTER_Y_HERO = 4; // au dessus du héro
+
+#endif
