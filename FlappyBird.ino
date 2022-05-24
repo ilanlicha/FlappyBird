462f2c65e84549c1570090e171d309df0dfeee3f
admin <Juliettemimenne42>
2022-05-24 16:30:04 +0200

    flappy rajout piece

--- FlappyBird.ino
+++ FlappyBird.ino
@@ -4,15 +4,25 @@
 #define DISPLAY_MODE DISPLAY_MODE_RGB565
 
 //image
+Image background(BACKGROUND_IMAGE_DATA);
+Image piece(PIECE_IMAGE_DATA);
 Image bird(IMAGE_DATA);
 Image tube(TUBE_IMAGE_DATA);
 Image tube2(TUBE2_IMAGE_DATA);
+
+//Background
+int background_positionX = 0;
+int background_positionY = 0;
+
+//Piece
+int piece_positionX = 10;
+int piece_positionY = 0;
 
 //bird
 bool dead = false;
 int birdPosX = 10;
 float birdPosY = gb.display.height()/2;
-int birdHeight = 11;
+int birdHeight = 12;
 int birdWidht = 19;
 float velocityY = 0;
 float gravity = .2;
@@ -22,6 +32,8 @@ void displayBird(){
     velocityY += gravity;
     birdPosY + velocityY + birdHeight >= gb.display.height() ? birdPosY = gb.display.height() - birdHeight : birdPosY += velocityY;    
   }
+  gb.display.drawImage(background_positionX, background_positionY, background);
+  gb.display.drawImage(piece_positionX, piece_positionY, piece);
   gb.display.drawImage(birdPosX, birdPosY, bird, birdWidht, birdHeight);
 }
 
