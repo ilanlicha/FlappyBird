8e0c431dcc1ac4f38b2dc0c0b671044f19ecbe0e
Iliass EL LAHIBI <iliassellahibi@gmail.com>
2022-05-24 18:53:08 +0200

    correction erreur - V1

--- FlappyBird.ino
+++ FlappyBird.ino
@@ -1,7 +1,21 @@
 #include <Gamebuino-Meta.h>
+#include <Vector.h>
 #include "assets/rgb565.h"
 
+using namespace std;
+
 #define DISPLAY_MODE DISPLAY_MODE_RGB565
+
+struct obstacle{
+  int posX;
+  int posY;
+  int size;
+
+  bool move(int, int);
+  void draw();
+};
+
+
 
 //image
 Image background(BACKGROUND_IMAGE_DATA);
@@ -27,6 +41,34 @@ int birdWidht = 19;
 float velocityY = 0;
 float gravity = .2;
 
+//tubes
+
+const int ELEMENT_COUNT_MAX = 50;
+obstacle storage_array[ELEMENT_COUNT_MAX];
+
+Vector<obstacle> obstacles(storage_array);
+
+int size_hole = 10;
+int distance_obstacles = 30;
+
+bool obstacle::move(int x, int y){
+  posY += y;
+  if( posX + x < (0 - 3) ){
+    return true;
+  }
+  posX += x;
+  return false;
+}
+
+void obstacle::draw(){
+  //tube up
+  //gb.display.fillRect(posX, 0, 3, posY - (size_hole/2));
+  gb.display.drawImage(posX, (40 - posY - (10/2)) * -1, tube);
+  //tube down
+  //gb.display.fillRect(posX, gb.display.height(), 3, ( (gb.display.height() - posY) - (size_hole/2)) * -1);
+  gb.display.drawImage(posX, posY + 10, tube2);
+}
+
 void displayBird(){
   if (!dead){
     velocityY += gravity;
@@ -39,12 +81,42 @@ void displayBird(){
 
 void setup() {
   gb.begin();
+
+  int holeY = random(10, 51);
+  obstacles.push_back((obstacle){ gb.display.width(), holeY, size_hole});
 }
 
 void loop() {
   while(!gb.update());
   gb.display.clear();
+
   displayBird();
+  
+  if(obstacles[obstacles.size() - 1].posX < gb.display.width() - distance_obstacles - 3){
+    int holeY = random(10, 51);
+    obstacles.push_back({ gb.display.width(), holeY, size_hole});
+    
+  }
+
+  for (int i = 0; i < obstacles.size(); i++)
+  {
+    gb.display.print(obstacles[i].posX);
+    obstacles[i].draw();
+  }
+
+  
+
+  for (int i = 0; i < obstacles.size(); i++)
+  {
+
+    if(obstacles[i].move(-1, 0)){
+      obstacles.remove(i);
+      i-=1;
+    }
+    
+  }
+
+  
   if (gb.buttons.pressed(BUTTON_A) && birdPosY-2 > 0){
     
     velocityY = -2;
